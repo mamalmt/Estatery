876c13f12178df9bc3b6bcdd35a6c40f9da5fe2d
mamalmt <tajikmohammadhasan0@gmail.com>
2023-01-15 09:16:43 +0330

    up bg

--- src/components/home/Hero/main/Main.jsx
+++ src/components/home/Hero/main/Main.jsx
@@ -2,15 +2,20 @@ import Headline from "./Headline";
 import CartSearch from "./CartSearch";
 import Numbers from "./Numbers";
 import Testimonial from "./Testimonial";
-const ba = require("../../../../assets/img/home-page/maine/Frame14265.png");
+
 function Main() {
   return (
     <>
-      <div
-        className=" flex flex-col gap-20  bg-none lg:gap-6  lg:bg-no-repeat lg:bg-right  "
-        style={{ backgroundImage: `url(${ba})` }}
-      >
-        <div className="lg:flex lg:justify-start ">
+      <div className=" flex flex-col gap-20  bg-none lg:gap-6  lg:bg-no-repeat lg:bg-right lg:-z-0 ">
+        <div className=" hiddenDiv lg:flex   lg:absolute  lg:mb-[100px] lg:ml-[800px] lg:-z-10">
+          <img
+            className="lg:w-[710px] lg:h-[630px] "
+            src={require("../../../../assets/img/home-page/maine/Frame14265.png")}
+            alt=""
+          />
+        </div>
+
+        <div className="lg:flex lg:justify-start lg:-z-0">
           <Headline />
           <Testimonial />
         </div>
